d4d6e6c2e9da899a0710fbbdddf5acebc1d13f59
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-08-05 17:55:46 -0400

    added signup.js in the public folder that adds new user

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+async function signupNewUser(event) {
+  event.preventDefault();
+  const name = document.getElementById("name-signup").value.trim();
+  const email = document.getElementById("email-signup").value.trim();
+  const password = document.getElementById("email-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/signup", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dasboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupNewUser);
