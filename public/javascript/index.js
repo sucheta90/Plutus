781438a1dfce1e0adca75885c8e13cb8477022d3
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-06 20:16:56 -0500

    charts and sub menue created

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -127,3 +127,28 @@ document.addEventListener('DOMContentLoaded', function() {
        }]
    });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const chart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
