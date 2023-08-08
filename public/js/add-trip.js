445f01dfa5ee7d74efd3e8873eea0c4aee851b49
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-08 10:37:59 -0500

    chart route created

--- public/js/add-trip.js
+++ public/js/add-trip.js
@@ -29,3 +29,127 @@ async function addTripExpense(event) {
 }
 
 document.querySelector('#submit-trip').addEventListener('click', addTripExpense);
+// ===============================================================================================
+const tripChart = async ()=> {
+  const response = await fetch('/api/trip')
+  const trips = await response.json()
+  console.log(trips[0].unit_cost);
+  const expenses = [
+    { description, amount},
+    { description, amount},
+    // Add more data objects here as needed
+  ];
+  
+  
+  // Create the chart
+  Highcharts.chart('container', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'trip',
+        align: 'left'
+    },
+    subtitle: {
+        text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>',
+        align: 'left'
+    },
+  
+    accessibility: {
+        announceNewData: {
+            enabled: true
+        },
+        point: {
+            valueSuffix: '%'
+        }
+    },
+  
+    plotOptions: {
+        series: {
+            borderRadius: 5,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}: {point.y:.1f}%'
+            }
+        }
+    },
+  
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+  
+    series: [
+        {
+            name: 'expenses',
+            colorByPoint: true,
+            data: [
+                {
+                    name: trips[0].description,
+                    y: 34.87,
+                    drilldown: trips[0].description
+                },
+                {
+                    name: 'Safari',
+                    y: 9.47,
+                    drilldown: 'Safari'
+                },
+                {
+                    name: 'Edge',
+                    y: 9.32,
+                    drilldown: 'Edge'
+                },
+                {
+                    name: 'Firefox',
+                    y: 8.15,
+                    drilldown: 'Firefox'
+                },
+                {
+                    name: 'Other',
+                    y: 11.02,
+                    drilldown: null
+                }
+            ]
+        }
+    ],
+    drilldown: {
+      series: [
+      {
+        name: 'drilldown',
+        data: expenses.map(expenses => ({ name: description, y: parseFloat(amount) })) // Use descriptions as names and amounts as data points
+      }
+    ]
+    }
+  });
+  // ======================================================================================
+  
+  
+  // Compile the Handlebars template
+  const template = Handlebars.compile(document.querySelector('#chart-template').innerHTML);
+  const compiledHtml = template();
+  
+  // Insert the compiled HTML into your document
+  document.querySelector('#container').innerHTML = compiledHtml;
+  
+  
+  // Create the Highcharts pie chart using the extracted data
+  const chart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'highcharts-container', // Use the container ID
+      type: 'pie' // Pie chart type
+    },
+    title: {
+      text: 'Expense Distribution by Description'
+    },
+    series: [
+      {
+        name: 'Expenses',
+        data: expenses.map(expenses => ({ name: description, y: parseFloat(amount) })) // Use descriptions as names and amounts as data points
+      }
+    ]
+  });
+}
+tripChart()
+
+
+
--- public/js/tripBudget.js
+++ public/js/tripBudget.js
@@ -1,156 +1,157 @@
-// Set to 00:00:00:000 today
-    let today = new Date();
+
+// // Set to 00:00:00:000 today
+//     let today = new Date();
     
-    const day = 1000 * 60 * 60 * 24,
-        dateFormat = Highcharts.dateFormat;
+//     const day = 1000 * 60 * 60 * 24,
+//         dateFormat = Highcharts.dateFormat;
     
-    // Set to 00:00:00:000 today
-    today.setUTCHours(0);
-    today.setUTCMinutes(0);
-    today.setUTCSeconds(0);
-    today.setUTCMilliseconds(0);
-    today = today.getTime();
+//     // Set to 00:00:00:000 today
+//     today.setUTCHours(0);
+//     today.setUTCMinutes(0);
+//     today.setUTCSeconds(0);
+//     today.setUTCMilliseconds(0);
+//     today = today.getTime();
     
-     cars = [{
-        model: 'activities',
-        current: 0,
-        deals: [{
-            rentedTo: 'sky diving',
-            from: today - 1 * day,
-            to: today + 2 * day
-        }, {
-            rentedTo: 'ATV',
-            from: today - 3 * day,
-            to: today - 2 * day
-        }, {
-            rentedTo: 'air ballooning',
-            from: today + 5 * day,
-            to: today + 6 * day
-        }]
-    }, {
-        model: 'Jaguar E-type',
-        current: 0,
-        deals: [{
-            rentedTo: 'Martin Hammond',
-            from: today - 2 * day,
-            to: today + 1 * day
-        }, {
-            rentedTo: 'Linda Jackson',
-            from: today - 2 * day,
-            to: today + 1 * day
-        }, {
-            rentedTo: 'Robert Sailor',
-            from: today + 2 * day,
-            to: today + 6 * day
-        }]
-    }, {
-        model: 'Volvo V60',
-        current: 0,
-        deals: [{
-            rentedTo: 'Mona Ricci',
-            from: today + 0 * day,
-            to: today + 3 * day
-        }, {
-            rentedTo: 'Jane Dockerman',
-            from: today + 3 * day,
-            to: today + 4 * day
-        }, {
-            rentedTo: 'Bob Shurro',
-            from: today + 6 * day,
-            to: today + 8 * day
-        }]
-    }, {
-        model: 'Volkswagen Golf',
-        current: 0,
-        deals: [{
-            rentedTo: 'Hailie Marshall',
-            from: today - 1 * day,
-            to: today + 1 * day
-        }, {
-            rentedTo: 'Morgan Nicholson',
-            from: today - 3 * day,
-            to: today - 2 * day
-        }, {
-            rentedTo: 'William Harriet',
-            from: today + 2 * day,
-            to: today + 3 * day
-        }]
-    }, {
-        model: 'Peugeot 208',
-        current: 0,
-        deals: [{
-            rentedTo: 'Harry Peterson',
-            from: today - 1 * day,
-            to: today + 2 * day
-        }, {
-            rentedTo: 'Emma Wilson',
-            from: today + 3 * day,
-            to: today + 4 * day
-        }, {
-            rentedTo: 'Ron Donald',
-            from: today + 5 * day,
-            to: today + 6 * day
-        }]
-    }];
+//      cars = [{
+//         model: 'activities',
+//         current: 0,
+//         deals: [{
+//             rentedTo: 'sky diving',
+//             from: today - 1 * day,
+//             to: today + 2 * day
+//         }, {
+//             rentedTo: 'ATV',
+//             from: today - 3 * day,
+//             to: today - 2 * day
+//         }, {
+//             rentedTo: 'air ballooning',
+//             from: today + 5 * day,
+//             to: today + 6 * day
+//         }]
+//     }, {
+//         model: 'Jaguar E-type',
+//         current: 0,
+//         deals: [{
+//             rentedTo: 'Martin Hammond',
+//             from: today - 2 * day,
+//             to: today + 1 * day
+//         }, {
+//             rentedTo: 'Linda Jackson',
+//             from: today - 2 * day,
+//             to: today + 1 * day
+//         }, {
+//             rentedTo: 'Robert Sailor',
+//             from: today + 2 * day,
+//             to: today + 6 * day
+//         }]
+//     }, {
+//         model: 'Volvo V60',
+//         current: 0,
+//         deals: [{
+//             rentedTo: 'Mona Ricci',
+//             from: today + 0 * day,
+//             to: today + 3 * day
+//         }, {
+//             rentedTo: 'Jane Dockerman',
+//             from: today + 3 * day,
+//             to: today + 4 * day
+//         }, {
+//             rentedTo: 'Bob Shurro',
+//             from: today + 6 * day,
+//             to: today + 8 * day
+//         }]
+//     }, {
+//         model: 'Volkswagen Golf',
+//         current: 0,
+//         deals: [{
+//             rentedTo: 'Hailie Marshall',
+//             from: today - 1 * day,
+//             to: today + 1 * day
+//         }, {
+//             rentedTo: 'Morgan Nicholson',
+//             from: today - 3 * day,
+//             to: today - 2 * day
+//         }, {
+//             rentedTo: 'William Harriet',
+//             from: today + 2 * day,
+//             to: today + 3 * day
+//         }]
+//     }, {
+//         model: 'Peugeot 208',
+//         current: 0,
+//         deals: [{
+//             rentedTo: 'Harry Peterson',
+//             from: today - 1 * day,
+//             to: today + 2 * day
+//         }, {
+//             rentedTo: 'Emma Wilson',
+//             from: today + 3 * day,
+//             to: today + 4 * day
+//         }, {
+//             rentedTo: 'Ron Donald',
+//             from: today + 5 * day,
+//             to: today + 6 * day
+//         }]
+//     }];
     
-    // Parse car data into series.
-    const series = cars.map(function (car, i) {
-        const data = car.deals.map(function (deal) {
-            return {
-                id: 'deal-' + i,
-                rentedTo: deal.rentedTo,
-                start: deal.from,
-                end: deal.to,
-                y: i,
-                name: deal.rentedTo
-            };
-        });
-        return {
-            name: car.model,
-            data: data,
-            current: car.deals[car.current]
-        };
-    });
+//     // Parse car data into series.
+//     const series = cars.map(function (car, i) {
+//         const data = car.deals.map(function (deal) {
+//             return {
+//                 id: 'deal-' + i,
+//                 rentedTo: deal.rentedTo,
+//                 start: deal.from,
+//                 end: deal.to,
+//                 y: i,
+//                 name: deal.rentedTo
+//             };
+//         });
+//         return {
+//             name: car.model,
+//             data: data,
+//             current: car.deals[car.current]
+//         };
+//     });
     
-    document.addEventListener('DOMContentLoaded', function () {
-        const chart = Highcharts.ganttChart('container', {
-        series: series,
-        plotOptions: {
-            series: {
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.name}',
-                    style: {
-                        fontWeight: 'normal'
-                    }
-                }
-            }
-        },
+//     document.addEventListener('DOMContentLoaded', function () {
+//         const chart = Highcharts.ganttChart('container', {
+//         series: series,
+//         plotOptions: {
+//             series: {
+//                 dataLabels: {
+//                     enabled: true,
+//                     format: '{point.name}',
+//                     style: {
+//                         fontWeight: 'normal'
+//                     }
+//                 }
+//             }
+//         },
         
-        xAxis: {
-            currentDateIndicator: true
-        },
-        yAxis: {
-            type: 'category',
-            grid: {
-                columns: [{
-                    title: {
-                        text: 'type expense'
-                    },
-                    categories: series.map(function (s) {
-                        return s.name;
-                    })
-                }, {
-                    title: {
-                        text: 'expense'
-                    },
-                    categories: series.map(function (s) {
-                        return s.current.rentedTo;
-                    })
-                }]
-            }
-        }
-    })});
+//         xAxis: {
+//             currentDateIndicator: true
+//         },
+//         yAxis: {
+//             type: 'category',
+//             grid: {
+//                 columns: [{
+//                     title: {
+//                         text: 'type expense'
+//                     },
+//                     categories: series.map(function (s) {
+//                         return s.name;
+//                     })
+//                 }, {
+//                     title: {
+//                         text: 'expense'
+//                     },
+//                     categories: series.map(function (s) {
+//                         return s.current.rentedTo;
+//                     })
+//                 }]
+//             }
+//         }
+//     })});
 //     // Set to 00:00:00:000 today
 // let today = new Date();
 
