a2404cf015d078de207a2a0795f7bb868c78c512
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-10-06 16:06:23 -0400

    added middleware to post routtes to /budget and /liabilities

--- controllers/api/budgetRoute.js
+++ controllers/api/budgetRoute.js
@@ -2,57 +2,74 @@ const router = require("express").Router();
 const { Budget, Item, MonthYear } = require("../../models");
 
 // posting budget
-router.post("/", async (req, res) => {
-  try {
-    const itemData = await Item.findOne({
-      where: { description: req.body.category },
-    });
-    const monthYearData = await MonthYear.findOne({
-      where: { month: req.body.month, year: req.body.year },
-    });
-    const monthYear = monthYearData.get({ plain: true });
-    const item = itemData.get({ plain: true });
-
-    const checkBudgetdata = await Budget.findOne({
-      where: {
-        monthYearId: monthYear.id,
-        userId: req.session.user_id,
-        itemId: item.id,
-      },
-    });
-    if (checkBudgetdata) {
-      const checkBudget = checkBudgetdata.get({ plain: true });
-      let amount =
-        parseFloat(checkBudget.budget_amount) + parseFloat(req.body.amount);
-      await Budget.update(
-        { budget_amount: amount },
-        {
-          where: {
-            monthYearId: monthYear.id,
-            userId: req.session.user_id,
-            itemId: item.id,
-          },
-        }
-      );
+router.post(
+  "/",
+  async (req, res, next) => {
+    if (
+      req.body.category &&
+      req.body.amount &&
+      req.body.month &&
+      req.body.year
+    ) {
+      next();
     } else {
-      const newBudgetItem = await Budget.create({
-        itemId: item.id,
-        budget_amount: req.body.amount,
-        userId: req.session.user_id,
-        monthYearId: monthYear.id,
-      });
-      //newBudgetItem.setUser(req.session.user_id);
-      if (!newBudgetItem) {
-        res.status(400).json("Something went wrong.. Please try again.");
-        return;
-      }
+      res
+        .status(400)
+        .json({ message: "Failed to gather information. Please try again!" });
     }
+  },
+  async (req, res) => {
+    try {
+      const itemData = await Item.findOne({
+        where: { description: req.body.category },
+      });
+      const monthYearData = await MonthYear.findOne({
+        where: { month: req.body.month, year: req.body.year },
+      });
+      const monthYear = monthYearData.get({ plain: true });
+      const item = itemData.get({ plain: true });
 
-    res.status(200).json("Success");
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
+      const checkBudgetdata = await Budget.findOne({
+        where: {
+          monthYearId: monthYear.id,
+          userId: req.session.user_id,
+          itemId: item.id,
+        },
+      });
+      if (checkBudgetdata) {
+        const checkBudget = checkBudgetdata.get({ plain: true });
+        let amount =
+          parseFloat(checkBudget.budget_amount) + parseFloat(req.body.amount);
+        await Budget.update(
+          { budget_amount: amount },
+          {
+            where: {
+              monthYearId: monthYear.id,
+              userId: req.session.user_id,
+              itemId: item.id,
+            },
+          }
+        );
+      } else {
+        const newBudgetItem = await Budget.create({
+          itemId: item.id,
+          budget_amount: req.body.amount,
+          userId: req.session.user_id,
+          monthYearId: monthYear.id,
+        });
+        //newBudgetItem.setUser(req.session.user_id);
+        if (!newBudgetItem) {
+          res.status(400).json("Something went wrong.. Please try again.");
+          return;
+        }
+      }
+
+      res.status(200).json("Success");
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
   }
-});
+);
 
 module.exports = router;
--- controllers/api/transactionRoute.js
+++ controllers/api/transactionRoute.js
@@ -17,58 +17,75 @@ router.post("/asset", async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.post("/liabilities", async (req, res) => {
-  try {
-    const itemData = await Item.findOne({
-      where: { description: req.body.category },
-    });
-    const monthYearData = await MonthYear.findOne({
-      where: { month: req.body.month, year: req.body.year },
-    });
-    const monthYear = monthYearData.get({ plain: true });
-    const item = itemData.get({ plain: true });
-    const itemizedBudgetData = await Budget.findOne({
-      where: {
-        userId: req.session.user_id,
-        monthYearId: monthYear.id,
-        itemId: item.id,
-      },
-    });
-    console.log("HERE IS THE ITEMIZED DATA", itemizedBudgetData);
-    if (itemizedBudgetData) {
-      console.log("INSIDE itemized BudgetADTA");
-      const itemizedBudget = itemizedBudgetData.get({ plain: true });
-      const newExpense = await Liabilities.create({
-        itemId: item.id,
-        amount: req.body.amount,
-        userId: req.session.user_id,
-        monthYearId: monthYear.id,
-        budgetId: itemizedBudget.id,
-      });
-      if (!newExpense) {
-        res.status(400).json(err);
-        return;
-      }
-      res.status(200).json(newExpense);
+router.post(
+  "/liabilities",
+  async (req, res, next) => {
+    if (
+      req.body.category &&
+      req.body.amount &&
+      req.body.month &&
+      req.body.year
+    ) {
+      next();
     } else {
-      console.log("ELSE STATEMENT itemized BudgetADTA");
-      const newExpense = await Liabilities.create({
-        itemId: item.id,
-        amount: req.body.amount,
-        userId: req.session.user_id,
-        monthYearId: monthYear.id,
-      });
-      if (!newExpense) {
-        res.status(400).json(err);
-        return;
-      }
-      res.status(200).json(newExpense);
+      res
+        .status(400)
+        .json({ message: "Failed to gather information. Please try again!" });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+  },
+  async (req, res) => {
+    try {
+      const itemData = await Item.findOne({
+        where: { description: req.body.category },
+      });
+      const monthYearData = await MonthYear.findOne({
+        where: { month: req.body.month, year: req.body.year },
+      });
+      const monthYear = monthYearData.get({ plain: true });
+      const item = itemData.get({ plain: true });
+      const itemizedBudgetData = await Budget.findOne({
+        where: {
+          userId: req.session.user_id,
+          monthYearId: monthYear.id,
+          itemId: item.id,
+        },
+      });
+      console.log("HERE IS THE ITEMIZED DATA", itemizedBudgetData);
+      if (itemizedBudgetData) {
+        console.log("INSIDE itemized BudgetADTA");
+        const itemizedBudget = itemizedBudgetData.get({ plain: true });
+        const newExpense = await Liabilities.create({
+          itemId: item.id,
+          amount: req.body.amount,
+          userId: req.session.user_id,
+          monthYearId: monthYear.id,
+          budgetId: itemizedBudget.id,
+        });
+        if (!newExpense) {
+          res.status(400).json(err);
+          return;
+        }
+        res.status(200).json(newExpense);
+      } else {
+        // console.log("ELSE STATEMENT itemized BudgetADTA");
+        const newExpense = await Liabilities.create({
+          itemId: item.id,
+          amount: req.body.amount,
+          userId: req.session.user_id,
+          monthYearId: monthYear.id,
+        });
+        if (!newExpense) {
+          res.status(400).json(err);
+          return;
+        }
+        res.status(200).json(newExpense);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
-});
+);
 router.delete("/:id", (req, res) => {
   res.json("You have reached the delete route in /transaction");
 });
